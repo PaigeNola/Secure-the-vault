18e33b48f6e69357c743e37df82387edd6b09681
Paige Cotignola <hello.paigenola@gmail.com>
2026-06-09 12:50:10 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let code1;
+code1 = 7 + 3;
+// console.log(code1);
+
+let code2 = 100 - 120 / 2;
+// console.log(code2);
+
+let code3 = (17 % 7) * 13;
+// console.log(code3);
+
+let message = "The vault has been secured. The combination is:";
+// console.log(message);
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+// console.log(codeA);
+
+let codeB = `${code1}-${code2}-${code3}`;
+// console.log(codeB);
+
+console.log(message, codeA, codeB);
